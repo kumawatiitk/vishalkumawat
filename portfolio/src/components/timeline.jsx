04514d4e42a897b7701f3d5f2ab14f7f09f558fa
Vishal Kumawat <vikumawa@microsoft.com>
2020-12-18 16:57:39 +0530

    portfolio

---
+++ portfolio/src/components/timeline.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Senior Software Engineer At Microsoft <span>2017 Nov-present</span></h2>
+                        <p>I am working on Microsoft Teams that's essential workplace software during covid situation</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Member of Teahnical Staff at ScaleGrid<span>2015-2017</span></h2>
+                        <p>ScaleGrid is management Solution for NoSQL database on public and private clouds. Provides one place console for provisioning/deprovisioning, Scale up/down, disaster recovery, Upgrade, Monitoring, backup/restore, Profiling database cluster. </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Application Developer at Oracle<span>2014-2015</span></h2>
+                        <p>As a fresher joined Oracle - Worked on the Applications Development Framework(ADF), which is a framework built on java, in Product Information Management(PIM) team in Fusion, SCM(Supply Chain Management).
+The Application on which we worked is a source for product data management across the enterprise.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
